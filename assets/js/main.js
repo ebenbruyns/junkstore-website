699f90cdb7dc1e2380bc8a51e2299c80b5a6bd63
mrs junkrunner <annie@sdk.co.nz>
2026-01-21 15:39:53 +1300

    Updates games data
    Updates game data including total games, storefront totals, featured games, ratings, and individual game details with retesting and backlog updates.
    Also includes the addition of several new images.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,42 +21,5 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  // Highlight.js for syntax highlighting
-  const script = document.createElement('script');
-  script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js";
-  script.onload = function() {
-    // Initialize highlight.js for syntax highlighting
-    hljs.highlightAll();
-  };
-  document.head.appendChild(script);
-
-  // Copy code function for the copy button
-  function copyCode(button) {
-    const codeBlock = button.previousElementSibling;
-    const codeText = codeBlock.textContent;
-
-    // Create a temporary textarea to copy the code
-    const textArea = document.createElement("textarea");
-    textArea.value = codeText;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-
-    // Optional: Show a success message after copying
-    button.textContent = "Copied!";
-    setTimeout(() => {
-      button.textContent = "Copy";
-    }, 2000);
-  }
-
-  // Attach copy functionality to the copy buttons
-  const copyButtons = document.querySelectorAll(".copy-btn");
-  copyButtons.forEach((button) => {
-    button.addEventListener("click", function () {
-      copyCode(this);
-    });
-  });
-
 });
 
